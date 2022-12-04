e7d7bdb9fe8ef7c3654c51e5d5b52e83a816c467
Larry Riley <larryriley12378@gmail.com>
2022-12-04 21:23:57 +0000

    loadbalancer

---
+++ lab13/main.tf
@@ -0,0 +1,145 @@
+
+variable "credentials_file" {
+  default = "/home/lar4749/cis-91-361423-9a20bb86a72d.json"
+}
+
+variable "project" {
+  default = "cis-91-361423"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  region      = var.region
+  zone        = var.zone
+  project     = var.project
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "cis91-network"
+}
+
+# Webserver instances
+resource "google_compute_instance" "webservers" {
+  count        = 6
+  name         = "web${count.index}"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+  labels = {
+    role: "web"
+  }
+}
+
+resource "google_compute_firewall" "default-firewall" {
+  name    = "default-firewall"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "3000", "5000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# Health Check
+resource "google_compute_health_check" "webservers" {
+  name = "webserver-health-check"
+
+  timeout_sec        = 1
+  check_interval_sec = 1
+
+  http_health_check {
+    port = 80
+  }
+}
+
+# Instance group
+resource "google_compute_instance_group" "webservers" {
+  name        = "cis91-webservers"
+  description = "Webserver instance group"
+
+  instances = google_compute_instance.webservers[*].self_link
+
+  named_port {
+    name = "http"
+    port = "80"
+  }
+}
+
+# Backend Service
+resource "google_compute_backend_service" "webservice" {
+  name      = "web-service"
+  port_name = "http"
+  protocol  = "HTTP"
+
+  backend {
+    group = google_compute_instance_group.webservers.id
+  }
+
+  health_checks = [
+    google_compute_health_check.webservers.id
+  ]
+}
+
+# URL Map: Everything to our one service
+resource "google_compute_url_map" "default" {
+  name            = "my-site"
+  default_service = google_compute_backend_service.webservice.id
+}
+
+# HTTP Proxy
+resource "google_compute_target_http_proxy" "default" {
+  name     = "web-proxy"
+  url_map  = google_compute_url_map.default.id
+}
+
+# Reserve our IP address
+resource "google_compute_global_address" "default" {
+  name = "external-address"
+}
+
+# Forwarding rule
+resource "google_compute_global_forwarding_rule" "default" {
+  name                  = "forward-application"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.default.id
+  ip_address            = google_compute_global_address.default.address
+}
+
+output "lb-ip" {
+  value = google_compute_global_address.default.address
+}
+
+output "external-ip" {
+  value = google_compute_instance.webservers[*].network_interface[0].access_config[0].nat_ip
+}
+
